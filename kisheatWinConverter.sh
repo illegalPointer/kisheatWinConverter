99e064dcb844697aa9cae015212ff4f3a9ac0ac8
illegalPointer <illegalPointer@users.noreply.github.com>
2015-06-02 12:37:43 +0200

    Update kisheatWinConverter.sh
    #Check if Image was created. if not, remove the kml File

--- kisheatWinConverter.sh
+++ kisheatWinConverter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Fix to naming problem with kisheat.py when working on Windows environments
+# Fix to naming problem with kisheat.py when trying to work on Windows environments
 # github.com/illegalPointer/kisheatWinConverter
 
 numArgs=1
@@ -23,17 +23,25 @@ do
     kmlNewImageName=$(echo "$line" | sed 's/:/_/g')
     newImageName=$(echo "$oldImageName" | sed 's/:/_/g')
     echo "$kmlNewImageName" >> "$kmlFile""TEMP"
-    mv $1$oldImageName $1$newImageName
+    #mv $1$oldImageName $1$newImageName
     ;;
    *)
     echo "$line" >> "$kmlFile""TEMP"
     ;;
   esac
  done
- echo "</kml>" >> "$kmlFile""TEMP"
- mv "$kmlFile""TEMP" "$kmlFile"
- newKmlFile=$(echo "$kmlFile" | sed 's/:/_/g')
- mv "$kmlFile" "$newKmlFile"
+ #Check if Image was created. if not, remove the kml File
+ if [ -f $1$oldImageName ]
+ then
+  mv $1$oldImageName $1$newImageName
+  echo "</kml>" >> "$kmlFile""TEMP"
+  mv "$kmlFile""TEMP" "$kmlFile"
+  newKmlFile=$(echo "$kmlFile" | sed 's/:/_/g')
+  mv "$kmlFile" "$newKmlFile"
+ else
+  rm "$kmlFile" 
+  rm "$kmlFile""TEMP"
+ fi
 done
 
 exit
